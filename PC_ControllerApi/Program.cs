9824c6e1fa8a2414f21fb3fa88f550b2562fae24
VitalityBooster2 <vitalik.oganyan89884076959@gmail.com>
2025-01-19 22:30:47 +0300

    #

--- PC_ControllerApi/Program.cs
+++ PC_ControllerApi/Program.cs
@@ -1,18 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using PC_ControllerApi;
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connection = builder.Configuration.GetConnectionString("DefaultConnection")!;
+
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
+
+var ap = builder.Services.BuildServiceProvider().GetService<ApplicationContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+};
 
 app.UseHttpsRedirection();
 
@@ -21,5 +41,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapGet("/", (context) => context.Response.WriteAsync("Hello world"));
+
 
 app.Run();
