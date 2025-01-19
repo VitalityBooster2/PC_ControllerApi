97bb6f2e412bf4d2dc65e8b3b24de6b73674f965
VitalityBooster2 <vitalik.oganyan89884076959@gmail.com>
2025-01-19 22:30:36 +0300

    aded configurations for models

---
+++ PC_ControllerApi/EntitiesConfigurations/BaseEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PC_ControllerApi.Models;
+
+namespace PC_ControllerApi.EntitiesConfigurations
+{
+    public class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(p => p.Id).HasName("id");
+            builder.Property(p => p.Id).HasColumnName("id").ValueGeneratedOnAdd();
+        }
+    }
+}
---
+++ PC_ControllerApi/EntitiesConfigurations/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PC_ControllerApi.EntitiesConfigurations;
+using PC_ControllerApi.Models;
+
+namespace PC_ControllerApi.EntityBuilders
+{
+    public class UserConfiguration<T> : BaseEntityConfiguration<T> where T : User
+    {
+        public override void Configure(EntityTypeBuilder<T> builder)
+        {
+            base.Configure(builder);
+            builder.Property(p => p.UserName).IsRequired().HasMaxLength(36).HasColumnName("user_name");
+            builder.Property(p => p.AdditionalInfo).HasColumnName("additional_info").HasMaxLength(300);
+            builder.Property(p => p.PC_Id).HasColumnName("pc_id").IsRequired();
+            builder.ToTable("user_info");
+        }
+    }
+}
