6827593c28f1497dd4c4f2e71ebb82fa51fc5ebe
VitalityBooster2 <vitalik.oganyan89884076959@gmail.com>
2025-01-19 22:30:12 +0300

    context added

---
+++ PC_ControllerApi/ApplicationContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PC_ControllerApi.EntitiesConfigurations;
+using PC_ControllerApi.EntityBuilders;
+using PC_ControllerApi.Models;
+using System.Diagnostics;
+
+namespace PC_ControllerApi
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<PC> PCes { get; set; }
+
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected ApplicationContext()
+        {
+           
+            Database.EnsureCreated();
+            
+        }
+
+        
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<User>(new UserConfiguration<User>().Configure);
+            modelBuilder.Entity<PC>(new PCConfiguration<PC>().Configure);
+
+            modelBuilder.Entity<User>();
+            modelBuilder.Entity<PC>();
+        }
+    }
+}
